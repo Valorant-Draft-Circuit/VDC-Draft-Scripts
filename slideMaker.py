f8fc93339cd0232b4d7828a3e36400aa2fb034b9
Logan Ramhap <44788545+loganramhap@users.noreply.github.com>
2022-05-11 18:35:09 -0400

    Update slideMaker.py

--- slideMaker.py
+++ slideMaker.py
@@ -1,15 +1,17 @@
 from dataStructurePullerThingy import populateFranchises, tierPulls, franchises, picks
 from pptx import Presentation
-from pptx.util import Inches
 
 def makeSlides():
     prs = Presentation()
-    prs.slide_width = Inches(16)
-    prs.slide_height = Inches(9)
     slide_layout = prs.slide_layouts[0]
     populateFranchises("VDCContracts-Teams.csv")
+    tierPulls("Season1VDCDraft_Board-Bot_CommandOutput.csv", "Contender")
+    tierPulls("Season1VDCDraft_Board-Bot_CommandOutput.csv", "Advanced")
+    tierPulls("Season1VDCDraft_Board-Bot_CommandOutput.csv", "Master")
+    tierPulls("Season1VDCDraft_Board-Bot_CommandOutput.csv", "Premier")
+    print(picks.keys())
+    print(picks["Contender"].keys())
     for tier in picks.keys():
-        tierPulls("Season1VDCDraft_Board-Bot_CommandOutput.csv", tier)
         title = prs.slides.add_slide(slide_layout)
         title.shapes.title.text = tier + " draft starts now"
         for pick in picks[tier].keys():
@@ -17,7 +19,6 @@ def makeSlides():
                 slide.placeholders[1].text = str(tier) + ": Round " + str(picks[tier][pick]['Round']) + ", Pick " + str(picks[tier][pick]['Pick']) + "\nFranchise: " + str(franchises[picks[tier][pick]["GM"]]['Franchise']) + "\nTeam: " + str(franchises[picks[tier][pick]["GM"]][tier])
                 slide.shapes.title.text = picks[tier][pick]["Player Selected"]
     
-    prs.save('vdcDraft_widescreen.pptx')
-    print("Done")
+    prs.save('vdcDraft.pptx')
     return None
 makeSlides()
